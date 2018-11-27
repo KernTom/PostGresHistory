d2ed57f7ffef03b9f93709648b93c88b2ad40670
KernTom <45398347+KernTom@users.noreply.github.com>
2018-11-27 17:26:47 +0100

    Create tableHistory.sql

---
+++ tableHistory.sql
@@ -0,0 +1,21 @@
+CREATE TABLE public.history
+(
+    oid bigint NOT NULL DEFAULT nextval('"GENERALS"."SEQ.CHANGELOG.History"'::regclass),
+    table_name character varying(120) COLLATE pg_catalog."default" NOT NULL,
+    column_name character varying(120) COLLATE pg_catalog."default" NOT NULL,
+    timestamp timestamp with time zone NOT NULL DEFAULT clock_timestamp(),
+    db_user_name character varying(120) COLLATE pg_catalog."default" NOT NULL DEFAULT "current_user"(),
+    app_name character varying(120) COLLATE pg_catalog."default" NOT NULL DEFAULT current_setting('application_name'::text),
+    old_value text COLLATE pg_catalog."default" NOT NULL DEFAULT ''::character varying,
+    new_value text COLLATE pg_catalog."default" NOT NULL DEFAULT ''::character varying,
+    column_oid bigint NOT NULL,
+    operation character varying(120) COLLATE pg_catalog."default" NOT NULL,
+    table_oid oid NOT NULL,
+    ordinal_position_of_column integer NOT NULL,
+    transaction_began timestamp with time zone NOT NULL DEFAULT transaction_timestamp(),
+    application_user bigint
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
